56b316f42ed49b5cb300cee4c6af68e67ff81b34
Hanif <abdurrahman.alhanif@gmail.com>
2023-12-16 21:19:15 +0700

    hide dashboard menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,9 +12,9 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->is('dashboard*')">
+                    {{-- <x-nav-link :href="route('dashboard')" :active="request()->is('dashboard*')">
                         {{ __('Dashboard') }}
-                    </x-nav-link>
+                    </x-nav-link> --}}
                     <x-nav-link :href="route('layanan')" :active="request()->is('layanan*')">
                         {{ __('Layanan') }}
                     </x-nav-link>
@@ -81,9 +81,9 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->is('dashboard*')">
+            {{-- <x-responsive-nav-link :href="route('dashboard')" :active="request()->is('dashboard*')">
                 {{ __('Dashboard') }}
-            </x-responsive-nav-link>
+            </x-responsive-nav-link> --}}
             <x-responsive-nav-link :href="route('layanan')" :active="request()->is('layanan*')">
                 {{ __('Layanan') }}
             </x-responsive-nav-link>
