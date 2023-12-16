67257f7d46ddf58dfb5881527e266c6464c44486
Hanif <abdurrahman.alhanif@gmail.com>
2023-12-16 11:36:17 +0700

    update mobile menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -81,8 +81,14 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+            <x-responsive-nav-link :href="route('dashboard')" :active="request()->is('dashboard*')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('layanan')" :active="request()->is('layanan*')">
+                {{ __('Layanan') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('berita')" :active="request()->is('berita*')">
+                {{ __('Berita') }}
             </x-responsive-nav-link>
         </div>
 
