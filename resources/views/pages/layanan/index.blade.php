7220d232162b97853dc57660607d1ec3d96b2a1c
Hanif <abdurrahman.alhanif@gmail.com>
2024-01-13 11:04:31 +0700

    add menu permohonan magang

--- resources/views/pages/layanan/index.blade.php
+++ resources/views/pages/layanan/index.blade.php
@@ -8,8 +8,8 @@
         ],
         [
             'images' => '/images/layanan-konsultasi.svg',
-            'nama' => 'Konsultasi',
-            'url' => '/layanan/konsultasi',
+            'nama' => 'Permohonan Magang',
+            'url' => '/layanan/permohonan-magang',
             'deskripsi' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum sapiente recusandae mollitia omnis alias possimus officia.',
         ],
         [
@@ -30,25 +30,25 @@
 
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="flex items-center font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="flex items-center text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
             Layanan
         </h2>
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <h3 class="font-bold text-3xl mb-3">Layanan kami</h3>
+                    <h3 class="mb-3 text-3xl font-bold">Layanan kami</h3>
                     <div
-                        class="grid grid-cols-2 lg:grid-cols-4 gap-x-3 gap-y-5 bg-gray-100/50 dark:bg-gray-700/50 backdrop-blur-lg rounded-lg p-5">
+                        class="grid grid-cols-2 p-5 rounded-lg lg:grid-cols-4 gap-x-3 gap-y-5 bg-gray-100/50 dark:bg-gray-700/50 backdrop-blur-lg">
                         {{-- @dd($layanan[0]['images']) --}}
                         @foreach ($layanan as $item)
                             <a href="{{ $item['url'] }}"
-                                class="rounded-md bg-white dark:bg-gray-700 overflow-hidden shadow-lg hover:-translate-y-2 hover:shadow-xl hover:shadow-green-500/20 hover:ring-2 ring-green-500 transition duration-200">
+                                class="overflow-hidden transition duration-200 bg-white rounded-md shadow-lg dark:bg-gray-700 hover:-translate-y-2 hover:shadow-xl hover:shadow-green-500/20 hover:ring-2 ring-green-500">
                                 <img src="{{ asset($item['images']) }}" alt="{{ $item['nama'] }}" height="200">
-                                <div class="text px-4 py-3">
-                                    <h4 class="font-bold text-xl">
+                                <div class="px-4 py-3 text">
+                                    <h4 class="text-xl font-bold">
                                         {{ $item['nama'] }}
                                     </h4>
                                     <p class="hidden md:inline-block dark:text-gray-400">
