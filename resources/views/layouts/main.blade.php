b64c39c3b9c6ee5938d5c2bca1c90a141d36de11
Hanif <abdurrahman.alhanif@gmail.com>
2024-01-15 09:25:50 +0700

    update about page

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,7 +10,7 @@
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=inter:400,600&display=swap" rel="stylesheet" />
+    <link href="https://fonts.bunny.net/css?family=inter:400,600|antic-didone:400&display=swap" rel="stylesheet" />
 
     {{-- Resource --}}
     @vite(['resources/css/app.css', 'resources/js/app.js', 'node_modules/@fortawesome/fontawesome-free/js/all.min.js'])
