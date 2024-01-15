6f5680747f9f750b33481d5233f62c5066954b8a
afif-whelly <211210058@student.mercubuana-yogya.ac.id>
2024-01-15 09:58:56 +0700

    ganti istilah jadi jenis data

--- resources/views/components/form-klaim-asuransi.blade.php
+++ resources/views/components/form-klaim-asuransi.blade.php
@@ -68,7 +68,7 @@
             </div>
         </div>
         <div>
-            <x-input-label for="kejadian">Kejadian</x-input-label>
+            <x-input-label for="kejadian">Jenis Data</x-input-label>
             <select name="kejadian" id="kejadian"
                 class="block w-full mt-1 truncate border-gray-300 rounded-md shadow-sm dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600">
                 @php
